fbb1f82cd7d25117b80781870707aa6ed0b9034d
Vadim <leprechaunvanrosenkreuz@gmail.com>
2019-09-17 15:30:48 +0300

    Email sending has been commented

--- backend/src/main/kotlin/com/kotlinspringvue/backend/controller/BackendController.kt
+++ backend/src/main/kotlin/com/kotlinspringvue/backend/controller/BackendController.kt
@@ -60,7 +60,8 @@ class BackendController() {
     @PreAuthorize("hasRole('USER')")
     fun sendSimpleEmail(): ResponseEntity<*> {
         try {
-            emailService.sendSimpleMessage(addressee, "Simple Email", "Hello! This is simple email")
+            //Uncomment to use
+            //emailService.sendSimpleMessage(addressee, "Simple Email", "Hello! This is simple email")
         } catch (e: Exception) {
             return ResponseEntity(ResponseMessage("Error while sending message"), HttpStatus.BAD_REQUEST)
         }
@@ -75,7 +76,8 @@ class BackendController() {
             var params:MutableMap<String, Any> = mutableMapOf()
             params["addresseeName"] = addressee
             params["signatureImage"] = "https://coderlook.com/wp-content/uploads/2019/07/spring-by-pivotal.png"
-            emailService.sendSimpleMessageUsingTemplate(addressee, "Template Email", "emailTemplate", params)
+            //Uncomment to use
+            //emailService.sendSimpleMessageUsingTemplate(addressee, "Template Email", "emailTemplate", params)
         } catch (e: Exception) {
             return ResponseEntity(ResponseMessage("Error while sending message"), HttpStatus.BAD_REQUEST)
         }
@@ -87,7 +89,8 @@ class BackendController() {
     @PreAuthorize("hasRole('USER')")
     fun sendHtmlEmail(): ResponseEntity<*> {
         try {
-            emailService.sendHtmlMessage(addressee, "HTML Email", "<h1>Hello!</h1><p>This is HTML email</p>")
+            //Uncomment to use
+            //emailService.sendHtmlMessage(addressee, "HTML Email", "<h1>Hello!</h1><p>This is HTML email</p>")
         } catch (e: Exception) {
             return ResponseEntity(ResponseMessage("Error while sending message"), HttpStatus.BAD_REQUEST)
         }
